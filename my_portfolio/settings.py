1b04067bdd089b22981979ce66c7452fb59cbd4f
Ahmed Rasheed <ehmedmughal@gmail.com>
2023-06-23 16:21:03 +0500

    check

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l&6d5t$a6xg1c3oy4g%9!^@_u#ob#3agus4f2mrk*i0^*4us45'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
